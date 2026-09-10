42462717ab33168cffa1b4e71f33a9455d949e46
Calum McVeigh <calum.mcveigh@gmail+github.com>
2026-09-10 13:54:50 +0100

    chore: cleaned up ingest script

--- scripts/ingest_statsbomb.py
+++ scripts/ingest_statsbomb.py
@@ -5,16 +5,7 @@ def main():
     matches = load_matches(competition_id=37, season_id=281)
     first_match_id = matches.iloc[0]["match_id"]
     events = load_events(match_id=first_match_id)
-
-    print("Matches:")
-    print(matches.head())
-
-    print("\nEvents:")
-    print(events.head())
-
-    print("\nEvent columns:")
-    print(events.columns.tolist())
-
+    
     DATA_DIR = Path("data/raw")
     DATA_DIR.mkdir(parents=True, exist_ok=True)
 
