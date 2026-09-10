eaacb3f5749d9f9344f0054a63d0df5c17fb93cf
Calum McVeigh <calum.mcveigh@gmail+github.com>
2026-09-10 13:58:18 +0100

    chore: updated ingest script to hold event data in its own folder

--- scripts/ingest_statsbomb.py
+++ scripts/ingest_statsbomb.py
@@ -9,8 +9,11 @@ def main():
     DATA_DIR = Path("data/raw")
     DATA_DIR.mkdir(parents=True, exist_ok=True)
 
+    EVENTS_DIR = Path("data/raw/events")
+    EVENTS_DIR.mkdir(parents=True, exist_ok=True)
+
     matches.to_parquet(DATA_DIR / "wsl_matches.parquet")
-    events.to_parquet(DATA_DIR / f"match_{first_match_id}_events.parquet")
+    events.to_parquet(EVENTS_DIR / f"match_{first_match_id}_events.parquet")
 
 if __name__ == "__main__":
     main()
