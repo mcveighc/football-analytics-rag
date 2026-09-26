19fe41baf25c1224ecf28474949b0529265a8164
Calum McVeigh <calum.mcveigh@gmail+github.com>
2026-09-26 17:13:49 +0100

    feat: updated match report to make it more markdown friendly

--- src/football_rag/reports/match_report.py
+++ src/football_rag/reports/match_report.py
@@ -6,35 +6,41 @@ def get_match_report(match_id: int, match_data: pd.DataFrame) -> str:
     away_team = indexed_match_data.loc[match_id, "away"]
     
     return str.join("\n", [
-        get_match_headline(home_team, away_team),
+        get_match_headline(match_id, home_team, away_team),
+        "",
         get_match_score_report(home_team, away_team),
         get_match_shots_report(home_team, away_team),
         get_match_xg_report(home_team, away_team)])
 
 
-def get_match_headline(home_team: pd.Series, away_team: pd.Series) -> str:
+def get_match_headline(match_id: int, home_team: pd.Series, away_team: pd.Series) -> str:
     match_date = home_team.get("match_date")
     home_team_name = home_team.get("team")
     away_team_name = away_team.get("team")
 
-    return f"{home_team_name} vs {away_team_name} - {match_date}"
+    title = f"# {home_team_name} vs {away_team_name}"
+    date = f"Date: {match_date}"
+    id = f"Match ID: {match_id}"
+    
+    return str.join("\n", [title, date, id])
+    
 
 def get_match_score_report(home_team: pd.Series, away_team: pd.Series) -> str:
     home_goals = home_team.get("goals")
     away_goals = away_team.get("goals")
     
-    return f"Score: {home_goals}-{away_goals}"
+    return f"- Score (from event data): {home_goals}-{away_goals}"
 
 def get_match_shots_report(home_team: pd.Series, away_team: pd.Series) -> str:
     home_shots = home_team.get("shots")
     away_shots = away_team.get("shots")
     
-    return f"Shots: {home_shots}-{away_shots}"
+    return f"- Shots: {home_shots}-{away_shots}"
 
 def get_match_xg_report(home_team: pd.Series, away_team: pd.Series) -> str:
     home_xg = home_team.get("total_xg")
     away_xg = away_team.get("total_xg")
     
-    return f"Expected goals: {home_xg:.2f}-{away_xg:.2f}"
+    return f"- Expected goals: {home_xg:.2f}-{away_xg:.2f}"
 
 
--- test/football_rag/reports/test_match_report.py
+++ test/football_rag/reports/test_match_report.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 def test_match_report_displays_expected_report():
     # assemble
+    match_id = 1
     match_date = "2023-11-05"
     home_team_name = "Manchester United W"
     away_team_name = "Brighton & Hove Albion WFC"
@@ -17,9 +18,9 @@ def test_match_report_displays_expected_report():
     away_xg = 1.9212
 
     match_summary_data = pd.DataFrame([
-        {"match_id": 1, "match_date": f"{match_date}", "team": f"{away_team_name}",
+        {"match_id": match_id, "match_date": f"{match_date}", "team": f"{away_team_name}",
             "home_or_away": "away", "goals": away_goals, "shots": away_shots, "total_xg": away_xg},
-        {"match_id": 1, "match_date": f"{match_date}", "team": f"{home_team_name}",
+        {"match_id": match_id, "match_date": f"{match_date}", "team": f"{home_team_name}",
             "home_or_away": "home", "goals": home_goals, "shots": home_shots, "total_xg": home_xg}
     ])
 
@@ -27,7 +28,10 @@ def test_match_report_displays_expected_report():
     output = get_match_report(1, match_summary_data).splitlines()
 
     # assert
-    assert output[0] == f"{home_team_name} vs {away_team_name} - {match_date}"
-    assert output[1] == f"Score: {home_goals:.0f}-{away_goals:.0f}"
-    assert output[2] == f"Shots: {home_shots:.0f}-{away_shots:.0f}"
-    assert output[3] == f"Expected goals: 1.65-1.92"
+    assert output[0] == f"# {home_team_name} vs {away_team_name}"
+    assert output[1] == f"Date: {match_date}"
+    assert output[2] == f"Match ID: {match_id}"
+    assert output[3] == ""
+    assert output[4] == f"- Score (from event data): {home_goals:.0f}-{away_goals:.0f}"
+    assert output[5] == f"- Shots: {home_shots:.0f}-{away_shots:.0f}"
+    assert output[6] == f"- Expected goals: 1.65-1.92"
